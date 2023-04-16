5464c8904653941e490ea0f13fbfcef8ea98ce66
Emmanuel Mura <emmanuel.mura@gmail.com>
2023-04-16 14:03:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demo Spring Boot Admin Server
+
+## Building Docker Image
+
+Run spring-boot build image plugin
+
+`mvn spring-boot:build-image -DskipTests`
---
+++ src/main/java/com/example/springbootadminserverdemo/SpringBootAdminServerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootadminserverdemo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerDemoApplication.class, args);
+	}
+}
